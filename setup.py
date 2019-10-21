9c70c72a67d1ba11c518138e46c48f2358dd23ae
Micha den Heijer <micha@michadenheijer.nl>
2019-10-21 21:48:35 +0200

    Fix install

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
-from distutiles.core import setup
+import setuptools
 
-setup(
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name="pynytimes",
     version="0.1-alpha",
     description="A Python wrapper for (most) New York Times APIs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Micha den Heijer",
     author_email="micha@michadenheijer.com",
     python_requires="~=3.4",
-    packages=["requests",],
+    packages=setuptools.find_packages(),
     license="MIT"
 )
