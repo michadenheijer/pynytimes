c98a993c79608130a139709551487d4c5556145b
Micha den Heijer <micha@michadenheijer.com>
2020-12-28 21:21:10 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     author="Micha den Heijer",
     author_email="micha@michadenheijer.com",
-    python_requires=">=3.5, <=3.9",
+    python_requires=">=3.5, <3.10",
     packages=setuptools.find_packages(),
     include_package_data = True,
     url="https://github.com/michadenheijer/pynytimes",
