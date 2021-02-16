29d8d5f17fef531b11322261b3358d28e8ac7946
Micha den Heijer <micha@michadenheijer.com>
2021-02-16 16:36:19 +0100

    Update __version__.py

--- pynytimes/__version__.py
+++ pynytimes/__version__.py
@@ -1,6 +1,6 @@
 __title__ = "pynytimes"
 __description__ = "A Python wrapper for (most) New York Times APIs"
-__url__ = "https://github.com/michadenheijer"
+__url__ = "https://github.com/michadenheijer/pynytimes"
 __version__ = "0.5.0"
 __author__ = "Micha den Heijer"
 __author_email__ = "micha@michadenheijer.com"
