10e78496d9441c0f037ff4c648a438bbe97aafa6
Micha den Heijer <micha@michadenheijer.com>
2021-07-14 14:24:21 +0200

    Bump version, remove Python 3.6 support, add Python 3.10 support

--- pynytimes/__version__.py
+++ pynytimes/__version__.py
@@ -2,7 +2,7 @@
 __title__ = "pynytimes"
 __description__ = "A Python wrapper for (most) New York Times APIs"
 __url__ = "https://github.com/michadenheijer/pynytimes"
-__version__ = "0.6.1"
+__version__ = "0.7.0"
 __author__ = "Micha den Heijer"
 __author_email__ = "micha@michadenheijer.com"
 __license__ = "MIT"
